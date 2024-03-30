5075545410b5f5e9a44f0a101e9eb9655df3a76a
Aaron Sinnott <aaronsinnott93@gmail.com>
2024-03-30 22:20:54 +0000

    Add languages app

---
+++ languages/models.py
@@ -0,0 +1,13 @@
+from typing import Self
+
+from django.db import models
+
+
+class Language(models.Model):
+    name = models.CharField(max_length=50, blank=False, null=False)
+    code = models.CharField(max_length=8, blank=False, null=False)
+    short_code = models.CharField(max_length=2, blank=False, null=False)
+    description = models.TextField(blank=False, null=False)
+
+    def __str__(self: Self) -> str:
+        return self.name
