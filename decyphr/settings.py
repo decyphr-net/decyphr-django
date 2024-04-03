342aa669d6ee488d64c0b9ee705be02ded5b1b87
Aaron Sinnott <aaronsinnott93@gmail.com>
2024-04-03 19:25:26 +0100

    Implement swagger and redoc

--- decyphr/settings.py
+++ decyphr/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "drf_yasg",
     "translate",
     "languages",
     "nlp",
